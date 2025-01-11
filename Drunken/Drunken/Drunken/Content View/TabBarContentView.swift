ec5b4b5ce0162d8763d7d4fc20a5fe5e303c0bf0
Suppanat Chinthumrucks <suppanat@siamsquared.com>
2025-01-12 01:31:46 +0700

    Add tab bar content view and remove unused file

---
+++ Drunken/Drunken/Drunken/Content View/TabBarContentView.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarContentView.swift
+//  Drunken
+//
+//  Created by Suppanat Chinthumrucks on 12/1/2568 BE.
+//
+
+import SwiftUI
+
+struct TabBarContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabBarContentView()
+}
--- Drunken/Drunken/Drunken/DrunkenApp.swift
+++ Drunken/Drunken/Drunken/DrunkenApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct DrunkenApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TabBarContentView()
         }
     }
 }
