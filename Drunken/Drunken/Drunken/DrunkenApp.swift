56801ef8e0aac088eb381b213523db0d1128aabc
jay-suppanat <119741800+jay-suppanat@users.noreply.github.com>
2025-01-12 01:17:24 +0700

    Init project

---
+++ Drunken/Drunken/Drunken/DrunkenApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrunkenApp.swift
+//  Drunken
+//
+//  Created by Suppanat Chinthumrucks on 12/1/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct DrunkenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
