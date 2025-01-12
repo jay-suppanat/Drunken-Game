8c0bc12a91a8ff94f0ec41a6440c81131e9982fc
Suppanat Chinthumrucks <suppanat@siamsquared.com>
2025-01-12 17:13:35 +0700

    Add service class

---
+++ Drunken/Drunken/Drunken/Service/DrunkenClient.swift
@@ -0,0 +1,10 @@
+//
+//  DrunkenClient.swift
+//  Drunken
+//
+//  Created by Suppanat Chinthumrucks on 12/1/2568 BE.
+//
+
+class DrunkenClient {
+    
+}
---
+++ Drunken/Drunken/Drunken/Service/DrunkenRouter.swift
@@ -0,0 +1,12 @@
+//
+//  DrunkenRouter.swift
+//  Drunken
+//
+//  Created by Suppanat Chinthumrucks on 12/1/2568 BE.
+//
+
+enum DrunkenRouter {
+    case settingMenuList
+
+
+}
