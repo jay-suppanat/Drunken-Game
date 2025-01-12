ce242702c8c9fcfc3b6f6e3340ddb0715faeb297
Suppanat Chinthumrucks <suppanat@siamsquared.com>
2025-01-12 17:31:12 +0700

    Add fetch json data function in drunken api client

--- Drunken/Drunken/Drunken/Service/DrunkenClient.swift
+++ Drunken/Drunken/Drunken/Service/DrunkenClient.swift
@@ -5,6 +5,22 @@
 //  Created by Suppanat Chinthumrucks on 12/1/2568 BE.
 //
 
-class DrunkenClient {
-    
+import SwiftUI
+
+class DrunkenClient: ObservableObject {
+    public func fetchData<T: Decodable>(response: T.Type, completion: @escaping (Result<T, Error>) -> Void) {}
+
+    public func fetchDataFromJSON<T: Decodable>(fileName: String, response: T.Type, completion: @escaping (Result<T, Error>) -> Void) {
+        guard let jsonFile = Bundle.main.url(forResource: fileName, withExtension: "json") else {
+            print("⚠️ \(fileName).json is not found.")
+            return
+        }
+
+        do {
+            let data = try JSONDecoder().decode(T.self, from: try Data(contentsOf: jsonFile))
+            completion(data as! Result<T, any Error>)
+        } catch {
+            print("Failed to decode \(fileName).json: \(error.localizedDescription)")
+        }
+    }
 }
