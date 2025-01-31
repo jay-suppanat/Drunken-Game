acec5490629ddbb465558222a4dfc80d6a0c71e0
Suppanat Chinthumrucks <suppanat@siamsquared.com>
2025-01-31 23:51:11 +0700

    Setup setting view model

--- Drunken/Drunken/Drunken/Content View/SettingContentView/SettingViewModel.swift
+++ Drunken/Drunken/Drunken/Content View/SettingContentView/SettingViewModel.swift
@@ -8,5 +8,31 @@
 import Foundation
 
 class SettingViewModel: ObservableObject {
-    
+    @Published private var settingMenuListPublished: SettingMenuListModel = SettingMenuListModel()
+
+    init() {
+        self.fetchSettingMenuList()
+    }
+}
+
+// MARK: Service
+
+extension SettingViewModel: Service {
+    private func fetchSettingMenuList() {
+        DrunkenService.fetchSettingMenuList { response in
+            self.settingMenuListPublished = response
+        }
+    }
+}
+
+// MARK: Get
+
+extension SettingViewModel: Get {
+    public func getSettingMenuListCount() -> Int {
+        return self.settingMenuListPublished.list.count
+    }
+
+    public func getSettingMenuAtIndex(_ index: Int) -> SettingMenuElement {
+        return self.settingMenuListPublished.list[index]
+    }
 }
